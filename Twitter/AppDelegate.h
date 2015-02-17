d07e5493fb19114f609a198c131d203fe99c2f93
Dhanu Agnihotri <dhanuagnihotri@gmail.com>
2015-02-16 20:54:10 -0800

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Dhanu Agnihotri on 2/16/15.
+//  Copyright (c) 2015 ___SocietyTech___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
